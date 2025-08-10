9e352f3c8f033a267bc2d5494715b1a010e8b122
Michael Bisbjerg <michael@mbwarez.dk>
2025-08-10 13:37:07 +0200

    Add NonTryMethods generator tests

---
+++ src/MBW.Generators.NonTryMethods.Tests/TestHelper.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MBW.Generators.NonTryMethods;
+using MBW.Generators.NonTryMethods.Abstracts.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace MBW.Generators.NonTryMethods.Tests
+{
+    internal static class TestHelper
+    {
+        public static IReadOnlyDictionary<string, string> Run(string source)
+        {
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            var references = new[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Task).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(GenerateNonTryMethodAttribute).Assembly.Location)
+            };
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                "Tests",
+                new[] { syntaxTree },
+                references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            ISourceGenerator generator = new AutogenNonTryGenerator();
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+            driver = driver.RunGenerators(compilation);
+            GeneratorDriverRunResult runResult = driver.GetRunResult();
+
+            return runResult.GeneratedSources.ToDictionary(x => x.HintName, x => x.SourceText.ToString());
+        }
+    }
+}
