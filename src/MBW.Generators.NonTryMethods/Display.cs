4f1eef53801497dbaffd14dd481d925eff9c09a3
Michael Bisbjerg <michael@mbwarez.dk>
2025-08-16 10:53:32 +0200

    Cleanup

--- src/MBW.Generators.NonTryMethods/Display.cs
+++ src/MBW.Generators.NonTryMethods/Display.cs
@@ -1,14 +1,9 @@
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MBW.Generators.NonTryMethods;
 
 internal static class Display
 {
-    public static readonly SymbolDisplayFormat Fqn = SymbolDisplayFormat.FullyQualifiedFormat;
-    public static readonly SymbolDisplayFormat Minimal = SymbolDisplayFormat.MinimallyQualifiedFormat;
-
     public static readonly SymbolDisplayFormat CrefFormat =
         new(
             globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.OmittedAsContaining,
@@ -21,15 +16,4 @@ internal static class Display
             miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers |
                                   SymbolDisplayMiscellaneousOptions.UseSpecialTypes
         );
-
-    public static NameSyntax? GlobalNamespaceName(INamespaceSymbol? ns)
-        => ns is null || ns.IsGlobalNamespace
-            ? null
-            : SyntaxFactory.ParseName(ns.ToDisplayString(Fqn));
-
-    public static TypeSyntax GlobalType(ITypeSymbol t)
-        => SyntaxFactory.ParseTypeName(t.ToDisplayString(Fqn));
-
-    public static string MinimalText(ISymbol s)
-        => s.ToDisplayString(Minimal);
 }
