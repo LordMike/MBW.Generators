0a71ef49e4632b5d4c427e4cebb161014def78a7
Michael Bisbjerg <michael@mbwarez.dk>
2025-08-22 14:44:19 +0200

    Restructure

---
+++ src/Common/MBW.Generators.Common/Polyfills/NotNullWhenAttribute.cs
@@ -0,0 +1,9 @@
+// ReSharper disable CheckNamespace
+
+namespace System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+internal sealed class NotNullWhenAttribute(bool returnValue) : Attribute
+{
+    public bool ReturnValue { get; } = returnValue;
+}
